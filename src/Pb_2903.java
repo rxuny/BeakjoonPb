4c7e42c413d137789f38a30b3f412e756971d2e7
roun Lee <dess6418@naver.com>
2025-05-30 21:36:59 +0900

    백준 구현 문제 풀이

--- src/Pb_2903.java
+++ src/Pb_2903.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Pb_2903 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int point = 2;
+        int count = 1;
+
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            point += count;
+            count = count * 2;
+        }
+        System.out.println(point * point);
+    }
 }

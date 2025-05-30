80be11bcaaec139824332937ca3d0dfd0a01fbe5
roun Lee <dess6418@naver.com>
2025-05-30 21:29:16 +0900

    백준 구현 문제 풀이

--- src/Pb_10798.java
+++ src/Pb_10798.java
@@ -6,12 +6,12 @@ public class Pb_10798 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        char[][] board = new char[5][15]; // 5개의 단어를 만드는데, 한 줄의 단어는 최대 15글자로 이루어진다.
+        char[][] board = new char[5][15];
 
         for (int i = 0; i < 5; i++) {
             String s = br.readLine();
             for (int j = 0; j < s.length(); j++) {
-                board[i][j] = s.charAt(j); // 한줄로 입력받은 문자열을 한 글자씩 char배열에 넣기
+                board[i][j] = s.charAt(j);
             }
         }
 
